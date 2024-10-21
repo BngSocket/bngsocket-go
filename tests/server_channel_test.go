ffa6080abf93b7bd64df97f99c8dafdfed967bd4
Fluffel₿uff ⚡️ <113269946+fluffelpuff@users.noreply.github.com>
2024-10-21 11:48:11 +0000

    add channel test server

--- tests/server_channel_test.go
+++ tests/server_channel_test.go
@@ -1,1 +1,60 @@
 package tests
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/CustodiaJS/bngsocket"
+)
+
+func acceptNewChannelRequests(channel *bngsocket.BngConnChannelListener) {
+	// Diese Schleife nimmt eintreffende Datensätze entgegegen
+
+}
+
+func serveConnServerChannel(conn net.Conn) {
+	// Die Verbindung wird geupgradet
+	bngsocket.DebugPrint("Verbindung upgraden")
+	upgrConn, err := bngsocket.UpgradeSocketToBngConn(conn)
+	if err != nil {
+		fmt.Println("Fehler beim Upgraden: " + err.Error())
+		return
+	}
+
+	// Es wird ein neuer Channel bereitgestellt
+	testChannel, err := upgrConn.OpenChannelListener("test-channel")
+	if err != nil {
+		bngsocket.DebugPrint(err.Error())
+		return
+	}
+
+	// Es wird auf neu Eintreffende Channel Anfragen geantwortet
+	go acceptNewChannelRequests(testChannel)
+}
+
+func TestChannelServer(t *testing.T) {
+	bngsocket.DebugSetPrintFunction(t.Log)
+	t.Log("Open UnixSocket")
+	os.Remove("/tmp/unixsock")
+	l, err := net.ListenUnix("unix", &net.UnixAddr{Name: "/tmp/unixsock", Net: "unix"})
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		return
+	}
+	defer l.Close()
+
+	for {
+		// Anfrage wird angenommen
+		t.Log("Wait of new conenction")
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		// Der RPC Server wird gestartet
+		go serveConnServerChannel(conn)
+	}
+}
--- tests/socket_testing.go
+++ tests/socket_testing.go
@@ -3,7 +3,6 @@ package tests
 import (
 	"bytes"
 	"fmt"
-	"sync"
 	"testing"
 
 	"github.com/CustodiaJS/bngsocket"
@@ -45,48 +44,5 @@ func client_channel_test(t *testing.T, upgrConn *bngsocket.BngConn) error {
 	}
 
 	// Es ist kein Fehler aufgetreten
-	return nil
-}
-
-func server_channel_test(t *testing.T, upgrConn *bngsocket.BngConn) error {
-	// Es wird ein neuer Channel bereitgestellt
-	testChannel, err := upgrConn.OpenChannelListener("test-channel")
-	if err != nil {
-		return err
-	}
-
-	wgt := new(sync.WaitGroup)
-	wgt.Add(1)
-
-	// Es wird eine Routine gestartet welche neue Anfrage auf dem Channel entgegen nimmt
-	go func() {
-		defer wgt.Done()
-
-		// Es wird auf neue Channel Joins gewartet
-		t.Log("Warte auf neue eingehende Channel anfrage")
-		chann, err := testChannel.Accept()
-		if err != nil {
-			panic(err)
-		}
-		t.Log("Server:", chann.GetSessionId())
-
-		// Es wird auf eintreffende Daten gewartet
-		t.Log("Server: Reading")
-		data := make([]byte, 4096)
-		r, err := chann.Read(data)
-		if err != nil {
-			t.Error("Server: " + err.Error())
-			t.Fail()
-		}
-		t.Log("Server: Reading done")
-
-		// Die Daten werden zurückgesendet
-		if i, err := chann.Write(data[:r]); i != len(data[:r]) {
-			panic("rewrite failed: " + err.Error())
-		}
-	}()
-
-	wgt.Wait()
-
 	return nil
 }
